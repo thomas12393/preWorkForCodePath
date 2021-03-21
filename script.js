7db267adbdb10278168100f9d1994f4602843e08
Glitch (ash-petalite-icon) <none>
2021-03-21 18:29:11 +0000

    🎗🌥 Updated with Glitch

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // global constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
-const cluePauseTime = 333; //how long to pause in between clues
+const cluePauseTime = 200; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
 //Global Variables
-var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var clueHoldTime = 1200; //how long to hold each clue's light/sound
+var pattern = [];
 var progress = 0;
+var playerStrike = 0;
 var gamePlaying = false;
 var tonePlaying = false;
 var volume = 1.0; //must be between 0.0 and 1.0
@@ -13,11 +14,15 @@ var guessCounter = 0;
 
 function startGame() {
   //initialize game variables
+  clueHoldTime = 1200
   progress = 0;
   gamePlaying = true;
+  playerStrike = 0;
+  randomPattern();
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
   playClueSequence();
+  
 }
 
 function stopGame() {
@@ -26,12 +31,26 @@ function stopGame() {
   document.getElementById("stopBtn").classList.add("hidden");
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function randomPattern(){
+  for(let p = 0;p < 10; p++){
+    pattern[p] = Math.floor(Math.random() * Math.floor(8)+1);
+  }
+}
+
 // Sound Synthesis Functions
 const freqMap = {
   1: 261.6,
   2: 329.6,
   3: 392,
-  4: 466.2
+  4: 466.2,
+  5: 539.4,
+  6: 572.3,
+  7: 599.9,
+  8: 636.3
 };
 
 function playTone(btn, len) {
@@ -88,6 +107,7 @@ function playClueSequence() {
     // for each clue that is revealed so far
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms");
     setTimeout(playSingleClue, delay, pattern[i]); // set a timeout to play that clue
+    clueHoldTime -= 20;
     delay += clueHoldTime;
     delay += cluePauseTime;
   }
@@ -111,8 +131,14 @@ function guess(btn) {
   
   // add game logic here
   if(btn!= pattern[guessCounter]){
-      loseGame();
-    }
+      if(playerStrike == 3){
+        loseGame();
+      }
+      else{
+        playerStrike +=1;
+        playClueSequence();
+      }
+    }   
   else{
     if(guessCounter < progress){
       guessCounter += 1
